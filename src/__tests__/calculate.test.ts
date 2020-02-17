a61611396c0969754d3e8cd5aa5d0a146bdfc98d
davetayls <dave.taylor@pogokid.com>
2020-02-17 18:57:56 +0000

    Implemented DMAS calculations from a string

--- src/__tests__/calculate.test.ts
+++ src/__tests__/calculate.test.ts
@@ -1,5 +1,4 @@
-import { calculate, isOperand, splitIntoCalculationParts } from '../calculate'
-import { split } from 'ts-node'
+import { calculate, isOperand, replacePartsWithResult, splitIntoCalculationParts } from '../calculate'
 
 describe('calculate', () => {
   describe('isOperand', () => {
@@ -19,17 +18,43 @@ describe('calculate', () => {
       expect(splitIntoCalculationParts('2*2-1')).toEqual([2, '*', 2, '-', 1])
     })
   })
-  describe('Single operator calculations', () => {
-    it.each([
-      ['2 + 2', 4],
-      ['2+2', 4],
-      ['2+ 2', 4],
-      ['2 +2', 4],
-      ['2 - 2', 0],
-      ['2 * 2', 4],
-      ['2 / 2', 1],
-    ])('should calculate %s to equal %s', function(calculation, expected) {
-      expect(calculate(calculation)).toEqual(expected)
+
+  describe('replaceWithResult', () => {
+    it('should replace the calculation at the beginning of the parts with the result', function() {
+      expect(replacePartsWithResult([2, '*', 3, '-', 1], 1, 6)).toEqual([6, '-', 1])
+    })
+    it('should replace the calculation at the end of the parts with the result', function() {
+      expect(replacePartsWithResult([2, '+', 2, '*', 3], 3, 6)).toEqual([2, '+', 6])
+    })
+    it('should replace the calculation in the middle of the parts with the result', function() {
+      expect(replacePartsWithResult([2, '+', 2, '*', 3, '-', 1], 3, 6)).toEqual([2, '+', 6, '-', 1])
+    })
+  })
+
+  describe('Given a calculation', () => {
+    describe('with a single operator', () => {
+      it.each([
+        ['2 + 2', 4],
+        ['2+2', 4],
+        ['2+ 2', 4],
+        ['2 +2', 4],
+        ['2 - 2', 0],
+        ['2 * 2', 4],
+        ['2 / 2', 1],
+      ])('%s should equal %s', function(calculation, expected) {
+        expect(calculate(calculation).result).toEqual(expected)
+      })
+    })
+    describe('with multiple operators', () => {
+      it.each([
+        ['2 + 2 * 3', 8],
+        ['2 + 2 * 2 * 2', 10],
+        ['2 - 2 + 2 * 2 / 2', 2],
+        ['2 - 2 + 2 / 0.5 * 2', 8],
+        ['8 * 2 - 1 * 8 / 2 + 4 * 8', 44],
+      ])('%s should equal %s', function(calculation, expected) {
+        expect(calculate(calculation).result).toEqual(expected)
+      })
     })
   })
 })
--- src/calculate.ts
+++ src/calculate.ts
@@ -6,14 +6,28 @@ export interface ICalculateResult {
 export type TOperand = '/' | '*' | '+' | '-'
 export type TCalculationPart = number | TOperand
 
-export const isOperand = (char: string): char is TOperand => /^[\/*+\-]$/.test(char)
-const isValid = (calculation: string) => /^[0-9\s\/\-+*]+[0-9]$/g.test(calculation)
+export const isOperand = (char: string | TCalculationPart): char is TOperand => /^[\/*+\-]$/.test(String(char))
+export const isNumber = (potentialNumber: any): potentialNumber is number => typeof potentialNumber == 'number'
 
+/**
+ * Is this calculation valid and only including the
+ * functionality we are implementing
+ */
+const isValid = (calculation: string) => /^[0-9\s\/\-+*.]+[0-9]$/g.test(calculation)
+
+/**
+ * Extract and parse a number from a particular part
+ * of a string
+ */
 const getNumber = (withoutSpaces: string) => (lastOperandIndex: number, currentIndex?: number) => {
   const lastNumberString = withoutSpaces.substring(lastOperandIndex + 1, currentIndex)
   return parseFloat(lastNumberString)
 }
 
+/**
+ * Split the calculation into it's number and operator parts
+ * so that it is easier to work with
+ */
 export const splitIntoCalculationParts = (calculation: string): any[] => {
   const withoutSpaces = calculation.replace(/\s/g, '')
   const getLastNumber = getNumber(withoutSpaces)
@@ -33,14 +47,66 @@ export const splitIntoCalculationParts = (calculation: string): any[] => {
   return parts
 }
 
-const findAndCalculate = (parts: TCalculationPart[], operand: TOperand): TCalculationPart[] => {}
+/**
+ * Replace the parts around the operand with the result
+ */
+export const replacePartsWithResult = (
+  parts: TCalculationPart[],
+  operandIndex: number,
+  result: number
+): TCalculationPart[] => {
+  const before = operandIndex > 0 ? parts.slice(0, operandIndex - 1) : []
+  const after = operandIndex + 2 < parts.length ? parts.slice(operandIndex + 2) : []
+  return [...before, result, ...after]
+}
 
+/**
+ * Find the operand and do the calculation, repeating the process until
+ * there are none left
+ */
+const findAndCalculate = (parts: TCalculationPart[], operands: TOperand[]): TCalculationPart[] => {
+  const findOperand = (part: TCalculationPart) => isOperand(part) && operands.includes(part)
+  const operandIndex = parts.findIndex(findOperand)
+  if (operandIndex > -1) {
+    const operand = parts[operandIndex] as TOperand
+    const left = parts[operandIndex - 1]
+    const right = parts[operandIndex + 1]
+    let result: number | undefined
+    if (isNumber(left) && isNumber(right)) {
+      switch (operand) {
+        case '/':
+          result = left / right
+          break
+        case '*':
+          result = left * right
+          break
+        case '+':
+          result = left + right
+          break
+        case '-':
+          result = left - right
+          break
+      }
+    }
+    if (isNumber(result)) parts = replacePartsWithResult(parts, operandIndex, result)
+    if (parts.find(findOperand)) {
+      return findAndCalculate(parts, operands)
+    }
+  }
+  return parts
+}
+
+/**
+ * Implementing DMAS of the BODMAS
+ */
 export const calculate = (calculation: string) => {
   if (isValid(calculation)) {
-    const parts = splitIntoCalculationParts(calculation)
-    // find first operator and replace numbers with result
-    const div = findAndCalculate(parts, '/')
-    return { result: 10 }
+    let parts = splitIntoCalculationParts(calculation)
+    parts = findAndCalculate(parts, ['/', '*'])
+    console.log(parts)
+    parts = findAndCalculate(parts, ['+', '-'])
+    console.log(parts)
+    return { result: parts[0] }
   } else {
     return { error: 'Only supports simple calculation' }
   }
